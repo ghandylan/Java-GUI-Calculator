91b49eaecbf7e85ffa732af16555148d988298ac
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2022-07-22 14:06:30 +0800

    new commit

---
+++ src/App/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+/**
+ *
+ * @author dylan
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/App/Numbers.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+/**
+ *
+ * @author dylan
+ */
+public class Numbers {
+
+    private int one;
+    private int two;
+    private int three;
+    private int four;
+    private int five;
+    private int six;
+    private int seven;
+    private int eight;
+    private int nine;
+    private int zero;
+
+    public int getOne() {
+        return 1;
+    }
+
+    public void setOne(int one) {
+        this.one = one;
+    }
+
+    public int getTwo() {
+        return 2;
+    }
+
+    public void setTwo(int two) {
+        this.two = two;
+    }
+
+    public int getThree() {
+        return 3;
+    }
+
+    public void setThree(int three) {
+        this.three = three;
+    }
+
+    public int getFour() {
+        return 4;
+    }
+
+    public void setFour(int four) {
+        this.four = four;
+    }
+
+    public int getFive() {
+        return 5;
+    }
+
+    public void setFive(int five) {
+        this.five = five;
+    }
+
+    public int getSix() {
+        return 6;
+    }
+
+    public void setSix(int six) {
+        this.six = six;
+    }
+
+    public int getSeven() {
+        return 7;
+    }
+
+    public void setSeven(int seven) {
+        this.seven = seven;
+    }
+
+    public int getEight() {
+        return 8;
+    }
+
+    public void setEight(int eight) {
+        this.eight = eight;
+    }
+
+    public int getNine() {
+        return 9;
+    }
+
+    public void setNine(int nine) {
+        this.nine = nine;
+    }
+
+    public int getZero() {
+        return 0;
+    }
+
+    public void setZero(int zero) {
+        this.zero = zero;
+    }
+}
